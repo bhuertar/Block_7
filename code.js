dc954afe3a0b7356b935ca378d275a92eb5ec8c4
BryanHuerta <bhuertar@DESKTOP-OUBEMJ8>
2024-03-04 14:29:06 -0600

    completed js assignment

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
-// 
+// 1. Add a variable associated with the value of the message
+// 2. Add alert syntax with value inside 
+// 3. Add a three variables associated with value of the vault number
+// 4. Create arithmatic operators to equal the vault numbers
+// 5. Test code as I proceed with assignment
+// 4. Add comments for important steps
+
+// Created a constant variable with the value of the message and alerted it
+const vaultMessage = `You have received this message because you have been chosen to open an important vault:`;
+
+// Created variables associated to each number of the vault
+const numOne = 100 / 10;
+const numTwo = 20+20;
+const numThree = 40-1;
+
+// Created an alert 
+alert(vaultMessage + " " + numOne + "-" + numTwo + "-" + numThree);
